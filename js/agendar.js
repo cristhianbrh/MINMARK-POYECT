fd08fe2e1bc67d5ef665f90df582056f4cfb307d
cristhianbrh <cristhianalexand@gmail.com>
2023-11-25 07:21:18 -0500

    Design total

--- js/agendar.js
+++ js/agendar.js
@@ -19,3 +19,20 @@ function viewDataClick(nameId, classNames, classNew = ""){
     const sectionUser_form_detailsUser = document.getElementById(nameId);
     sectionUser_form_detailsUser.className = `${classNames} ${classNew}`;
 }
+
+
+
+
+// function onclickInTable_User(e){
+//     const input_DNI = document.getElementById("input_DNI")
+//     const input_Nombre = document.getElementById("input_Nombre")
+//     const input_Correo = document.getElementById("input_Correo")
+//     const input_Select_Rol = document.getElementById("input_Select_Rol")
+//     const input_Select_Estado = document.getElementById("input_Select_Estado")
+
+//     console.log(e)
+//     console.log("Lol")
+
+
+
+// }
